e1c4fd108f43d631a655436fa30867da66e2aac6
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-08-15 17:57:47 -0600

    added generate PW function, if stmts and for loop

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ var upperCs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCs = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var splChars = "!#%$&'()*+,-./:;<=>?@[\]^_`{|}~";
-var passwordlgth;
+var passwordLgth;
 var uppercaseCheck = false;
 var numberCheck = false;
 var splCheck = false;
 
 // functions that will guide password length
 function pwLength () {
-  passwordlgth = prompt("Select password length (must be between 8-128 characters");
+  passwordLgth = prompt("Select password length (must be between 8-128 characters)");
 
-  if (passwordlgth < 8) {
+  if (passwordLgth < 8) {
     alert("Password minimum is 8 characters long");
     pwLength();
   }
-  else if (passwordlgth > 128) {
+  else if (passwordLgth > 128) {
     alert("password cannot be more than 128 characters")
     pwLength();
   }
   else { 
 alert("Select character types")
-    return passwordlgth;
+    return passwordLgth;
 
   }
 }
@@ -38,9 +38,53 @@ function uppercase() {
   return numberCheck;
  }
  function determineSpecial(){
-   splChars = confirm("Do you want to include special characters?")
-   return splChars;
+   splCheck = confirm("Do you want to include special characters?")
+   return splCheck;
  }
+
+//  create function to generate password 
+
+function generatePassword(){
+  pwLength();
+  uppercase();
+  determineNumbers();
+  determineSpecial();
+
+  var characters = lowerCs;
+    var password = "";
+    if (!uppercaseCheck && !numberCheck && !splCheck) {
+        return;
+
+    } 
+    if (uppercaseCheck) {
+        characters += upperCs 
+;
+
+    } 
+     if (numberCheck) {
+        characters += numbers;
+;
+
+    } if (uppercaseCheck) {
+        characters += upperCs;
+;
+    }
+
+     
+    if (splCheck) {
+        characters += splChars;
+
+    } 
+
+    for (var i = 0; i < passwordLgth; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return password;
+
+
+
+
+}
 
 
 
