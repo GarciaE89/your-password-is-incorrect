cbea720a909534a98141149aab39eddd6c76f074
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-08-15 17:01:36 -0600

    added functions asking if they want special characters, numbers and uppercase

--- script.js
+++ script.js
@@ -4,8 +4,43 @@ var lowerCs = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var splChars = "!#%$&'()*+,-./:;<=>?@[\]^_`{|}~";
 var passwordlgth;
-var
+var uppercaseCheck = false;
+var numberCheck = false;
+var splCheck = false;
 
+// functions that will guide password length
+function pwLength () {
+  passwordlgth = prompt("Select password length (must be between 8-128 characters");
+
+  if (passwordlgth < 8) {
+    alert("Password minimum is 8 characters long");
+    pwLength();
+  }
+  else if (passwordlgth > 128) {
+    alert("password cannot be more than 128 characters")
+    pwLength();
+  }
+  else { 
+alert("Select character types")
+    return passwordlgth;
+
+  }
+}
+
+
+// function will ask end user if they want upper case letters, numbers and special characters
+function uppercase() {
+  uppercaseCheck = confirm("Do you want to include uppercase letters?");
+  return uppercaseCheck;
+}
+ function determineNumbers() {
+   numberCheck = confirm("Do you want include numbers?")
+  return numberCheck;
+ }
+ function determineSpecial(){
+   splChars = confirm("Do you want to include special characters?")
+   return splChars;
+ }
 
 
 
